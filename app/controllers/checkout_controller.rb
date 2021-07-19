305df350f97717e63a739f8aa5713681761af86a
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-07-19 22:30:26 +0800

    metadata

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -5,17 +5,24 @@ class CheckoutController < ApplicationController
         Stripe.api_key = Rails.application.credentials.stripe[:secret_key]
         @session = Stripe::Checkout::Session.create({
             payment_method_types: ['card'],
+
             line_items: [{
-                name: @product.id,
+                name: @product.name,
                 amount: @product.price,
                 currency: 'usd',
-                quantity: 1
+                quantity: 1,
             }],
+            payment_intent_data: {
+                metadata: {
+                    product_id: @product.id,
+                    price: @product.price
+                }
+            },
             mode: 'payment',
             success_url: root_url,
             cancel_url: root_url
           })
-          
+
           respond_to do |format|
             format.js
             end
