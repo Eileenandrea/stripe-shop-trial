126f120d35fa3b21e544b81087f9046bb1855a8b
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-07-19 18:05:25 +0800

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   post  'checkout', to: 'checkout#create', as: 'checkout_create'
   resources :products
   root "products#index"
-  resources webhooks, only: [:create]
+  resources :webhooks, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
