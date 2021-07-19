e2325fc30333cbb93506f1a28ababf8ad14ec319
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-07-19 17:20:33 +0800

    stripe checkout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  post  'checkout', to: 'checkout#create', as: 'checkout_create'
   resources :products
   root "products#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
